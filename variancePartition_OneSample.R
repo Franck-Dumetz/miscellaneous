04dc10c419591c2ab087373f7005011e9eb294d6
Franck Dumetz <105318680+Franck-Dumetz@users.noreply.github.com>
2025-07-09 15:09:24 -0400

    Create variancePartition_OneSample.R

---
+++ variancePartition_OneSample.R
@@ -0,0 +1,46 @@
+if (!requireNamespace("variancePartition", quietly = TRUE)) {
+  install.packages("BiocManager")
+  BiocManager::install("variancePartition")
+}
+
+library(variancePartition)
+library(edgeR)         # optional for CPM calculation
+library(limma)         # needed for voom
+library(tidyverse)
+
+# read expression data
+expr <- read.delim("/Users/fdumetz/Desktop/Desktop - fdumetzm4-osx/Ld1S_UTR/PTU_stat/transcript_counts_newgff.txt")
+
+# read gene features
+features <- read.delim("/Users/fdumetz/Desktop/Desktop - fdumetzm4-osx/Ld1S_UTR/PTU_stat/gene_features.tsv")
+
+# merge
+dat <- merge(expr, features, by = "gene_id")
+
+dat$PTU_ID <- as.factor(dat$PTU_ID)
+dat$position_in_PTU <- as.numeric(dat$position_in_PTU)
+dat$gene_length <- as.numeric(dat$gene_length)
+
+# set gene_id as rownames
+exprMat <- as.matrix(dat$CPM)
+rownames(exprMat) <- dat$gene_id
+colnames(exprMat) <- "CPM"
+y <- dat$CPM
+
+# Define the model
+form <- ~ (1|PTU_ID) + gene_length + position_in_PTU #(1|PTU_ID) → random effect, gene_length → fixed effect, position_in_PTU → fixed effect
+
+# Fit the model
+varPart <- fitExtractVarPartModel(
+  y,
+  form,
+  dat
+)
+
+# calculate average variance explained
+avgVar <- colMeans(varPart)
+
+# Show results
+print(varPart)
+plotVarPart(varPart)
+print(avgVar)
